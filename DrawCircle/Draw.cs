76a29f8173a7c25a957d3820051d04aeb2da07cc
brsyldrm <brs.yildirim0@gmail.com>
2023-01-21 13:12:31 +0300

    .

---
+++ DrawCircle/Draw.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawCircle
+{
+    public class Draw
+    {
+        public void Circle(int radius)
+        {
+            for (double y = -radius; y <= radius; y++)
+            {
+                for (double x = -radius; x <= radius; x += 0.5)
+                {
+                    if ((x * x) + (y * y) >= radius * radius)
+                        Console.Write(" ");
+                    else
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DrawCircle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw draw = new Draw();
+            Console.WriteLine("Yarı çap giriniz.");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            draw.Circle(radius);
+            Console.ReadKey();
+        }
+    }
+}

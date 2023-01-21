1dd0e6e2da4d8628b56e9a461ca4ef2d078ef6c0
brsyldrm <brs.yildirim0@gmail.com>
2023-01-21 12:59:15 +0300

    .

---
+++ DrawTriangle/Draw.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTriangle
+{
+    public class Draw
+    {
+        public void drawTri(int size)
+        {
+            for (int i = 0; i <= size; i++)
+            {
+                for (int j = 0; j <i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DrawTriangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw draw = new Draw();
+            Console.WriteLine("Yükseklik giriniz.");
+            int size = Convert.ToInt32(Console.ReadLine());
+            draw.drawTri(size);
+            Console.ReadKey();
+        }
+    }
+}

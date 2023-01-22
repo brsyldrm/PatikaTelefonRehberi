2a6882f5bd5dfb4c60e6018b0f4c2e4d74e8da76
brsyldrm <brs.yildirim0@gmail.com>
2023-01-23 00:06:49 +0300

    .

---
+++ MutlakKareAlma/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutlakKareAlma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayıları giriniz.");
+            string input = Console.ReadLine();
+            string[] strings = input.Split(' ');
+            int[] numbers = new int[strings.Length];
+
+            int farkToplam = 0;
+            int farkKareToplam = 0;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                int x = int.Parse(strings[i]);
+                numbers[i] = x;
+                if (numbers[i] <= 67)
+                {
+                    farkToplam += 67 - numbers[i];
+                }
+                else
+                {
+                    int kare = numbers[i]-67;
+                    farkKareToplam += (kare*kare);
+
+                }
+            }
+
+            Console.WriteLine(" 67'den Küçük olanların farklarının toplamı :" + farkToplam);
+            Console.WriteLine("67'den büyük farkların mutlak karelerini alarak toplamı : " + farkKareToplam);
+        }
+    }
+}
